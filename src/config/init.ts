c4157c00fd0abb769e7ed0085b96da48a6eefd9c
zelvan <zelvan1701@gmail.com>
2024-04-04 15:26:39 +0700

    style: restructure file

---
+++ src/config/init.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var _db: PrismaClient | undefined;
+}
+
+if(!global._db) {
+  global._db = new PrismaClient();
+}
+
+const db: PrismaClient = global._db;
+
+export default db;
