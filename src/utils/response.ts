f7de232d4bf175bc78522606ceca21ba421c0fcf
zelvan <zelvan1701@gmail.com>
2024-04-08 21:49:13 +0700

    feat: add cart controller and response util

---
+++ src/utils/response.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import { Response } from "express";
+
+export const responseOK = (res: Response, message: string, data: any) => {
+  return res.status(200).json({
+    success: true,
+    message: message,
+    data: data
+  });
+}
