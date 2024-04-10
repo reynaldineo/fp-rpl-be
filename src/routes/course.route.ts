0c84869c52789a8e7a52d34d7fa014d574bef488
zelvan <zelvan1701@gmail.com>
2024-04-10 22:55:09 +0700

    feat: add course endpoint api documentation

--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -14,14 +14,14 @@ export class CourseRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}`, AuthMiddleware, this.user.getCourse);
-    this.router.get(`${this.path}/:username`, AuthMiddleware, this.user.myCourse);
-    this.router.get(`${this.path}/detail/:id`, AuthMiddleware, this.user.getCourseByID);
+    this.router.get(`${this.path}/:username`, AuthMiddleware, this.user.getByUsername);
+    this.router.get(`${this.path}/detail/:id`, AuthMiddleware, this.user.getByID);
     this.router.get(`${this.path}/detail/:id/getLikes`, AuthMiddleware, this.user.getUserLikesCourse);
     this.router.get(`${this.path}/detail/:id/productList`, AuthMiddleware, this.user.getCourseProd);
     this.router.post(`${this.path}/create`, AuthMiddleware, this.user.addCourse);
-    this.router.post(`${this.path}/detail/:id/doLike`, AuthMiddleware, this.user.tapLike); // like or unlike
-    this.router.post(`${this.path}/detail/:id/doComment`, AuthMiddleware, this.user.addComment);
-    this.router.put(`${this.path}/detail/:id/edit`, AuthMiddleware, this.user.editCourse);
+    this.router.post(`${this.path}/detail/:id/tapLike`, AuthMiddleware, this.user.tapLike); // like or unlike
+    this.router.post(`${this.path}/detail/:id/addComment`, AuthMiddleware, this.user.addComment);
+    this.router.put(`${this.path}/detail/:id/update`, AuthMiddleware, this.user.editCourse);
     this.router.delete(`${this.path}/detail/:id/delete`, AuthMiddleware, this.user.deleteCourse);
   }
 }
