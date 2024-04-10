1cfc31cf7122be15746b995dbe2ada4eedeb6349
zelvan <zelvan1701@gmail.com>
2024-04-10 15:37:07 +0700

    feat: add course route

--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Routes } from "../interfaces/routes.interface.js";
 import { CourseController } from "../controllers/course.controller.js";
+import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 
 export class CourseRoute implements Routes {
   public path = "/course";
@@ -12,7 +13,14 @@ export class CourseRoute implements Routes {
   }
 
   private initializeRoutes() {
-    //this.router.post(`${this.path}/sample/:number(\\d+)`, ValidationMiddleware(SampleTestDTO, true), this.user.SampleTest);
-    // Define your routes here
+    this.router.get(`${this.path}`, AuthMiddleware, this.user.getCourse);
+    this.router.get(`${this.path}/:id`, AuthMiddleware, this.user.getCourseByID);
+    this.router.get(`${this.path}/:id/getLikes`, AuthMiddleware, this.user.getUserLikesCourse);
+    this.router.get(`${this.path}/:id/productList`, AuthMiddleware, this.user.getCourseProd);
+    this.router.post(`${this.path}/create`, AuthMiddleware, this.user.addCourse);
+    this.router.post(`${this.path}/:id/doLike`, AuthMiddleware, this.user.tapLike); // like or unlike
+    this.router.post(`${this.path}/:id/doComment`, AuthMiddleware, this.user.addComment);
+    this.router.put(`${this.path}/:id/edit`, AuthMiddleware, this.user.editCourse);
+    this.router.delete(`${this.path}/:id/delete`, AuthMiddleware, this.user.deleteCourse);
   }
 }
